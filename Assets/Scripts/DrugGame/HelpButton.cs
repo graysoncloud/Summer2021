5a225f52e9f9d2073d8bf575cc807bf4b4641298
HCEspino <harrison.espino@gmail.com>
2021-08-26 22:39:43 -0700

    Help Menu Finished

--- Assets/Scripts/DrugGame/HelpButton.cs
+++ Assets/Scripts/DrugGame/HelpButton.cs
@@ -1,19 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class HelpButton : MonoBehaviour
 {
     // Start is called before the first frame update
 
     public GameObject helpMenu;
+    public GameObject textObj;
+    private TextMeshProUGUI textTMP;
+
+    void Start(){
+        textTMP = textObj.GetComponent<TextMeshProUGUI>();
+        textTMP.text = "Help";
+    }
     void OnMouseUp(){
         if(helpMenu.activeSelf){
             helpMenu.SetActive(false);
+            textTMP.text = "Help";
         }
         else
         {
             helpMenu.SetActive(true);
+            textTMP.text = "Back";
         }
     }
 }
